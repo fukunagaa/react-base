0d78d7ccc0c606fa0000ed4a1927d4488ca35d9e
fukunagaa <k.k.0627ku@gmail.com>
2020-07-14 16:10:34 +0900

    Conponent thisの扱い

--- src/Layaut.js
+++ src/Layaut.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-function Layout() {
-    let name = "TAKESHI";
-  return (
-    <div>
-      <h1>React Welcome !!</h1>
-      <h2>add conponent1</h2>
-      <h2>{name} add conponent2 </h2>
-    </div>
-  );
+class Layout extends React.Component {
+  constructor() {
+    super();
+    this.name = "TAKESHI";
+  }
+  render() {
+    return (
+      <div>
+        <h1>React Welcome !!</h1>
+        <h2>add conponent1</h2>
+        <h2>{this.name} add conponent2 </h2>
+      </div>
+    );
+  }
 }
 
 export default Layout;
