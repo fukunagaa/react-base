604116b55a0dcf3fa4f1d8d5fbb75858eebde706
fukunagaa <k.k.0627ku@gmail.com>
2020-07-14 15:18:53 +0900

    Component化まで

---
+++ src/Layaut.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Layout() {
+  return (
+    <h1>React Welcome !!</h1>
+  );
+}
+
+export default Layout;
--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const app = document.getElementById("app");
+import Layout from "./Layaut";
 
-class Layout extends React.Component {
-    render() {
-        return (
-            <h1>React Welcome !!</h1>
-        );
-    }
-};
+const app = document.getElementById("app");
 
 ReactDOM.render(<Layout />, app);
