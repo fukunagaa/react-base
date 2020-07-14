0517c1d9586ddde11e70af4241959258456dd6f3
fukunagaa <k.k.0627ku@gmail.com>
2020-07-14 16:04:19 +0900

    変数の扱いを追加

--- src/Layaut.js
+++ src/Layaut.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 function Layout() {
+    let name = "TAKESHI";
   return (
-    <h1>React Welcome !!</h1>
+    <div>
+      <h1>React Welcome !!</h1>
+      <h2>add conponent1</h2>
+      <h2>{name} add conponent2 </h2>
+    </div>
   );
 }
 
